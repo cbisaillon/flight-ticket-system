9d176dd249dced1dd88debef3823ef4604429f74
Clement <clement.bisaillon@hotmail.com>
2021-04-25 15:30:57 -0400

    Worked on search controller, created test

---
+++ resources/js/components/FlightDescription.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    asdf
+    <table>
+      <!-- From -->
+      <tr>
+        <td>{{ flight.departure.departure_date }}</td>
+        <td>{{ flight.departure.origin_airport.city }} ({{ flight.departure.origin_airport.code }})</td>
+      </tr>
+      <!-- To -->
+      <tr>
+        <td>{{ flight.departure.arrival_date }}</td>
+        <td>{{ flight.departure.arrival_airport.city }} ({{ flight.departure.arrival_airport.code }})</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+name: "FlightDescription",
+  props: {
+    flight: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
